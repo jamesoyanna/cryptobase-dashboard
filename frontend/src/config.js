8e49125ac01f70a5c0918ee4fda195f6c38aea72
James <engrjayt200@gmail.com>
2022-02-24 22:15:37 +0100

    Bug fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+//  baseURL: "https://cryptobase-backend.herokuapp.com"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://cryptobase-backend.herokuapp.com"
+    baseURL: "https://crypto-base-backend-app.herokuapp.com"
 })
+
+// https://crypto-base-backend-app.herokuapp.com/
